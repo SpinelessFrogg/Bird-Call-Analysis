a398f23ae4d23fc10bc473b524f9723186f727ea
Caleb <spinelessfrog@gmail.com>
2026-02-09 20:02:25 -0600

    refactoring main into scripts, moved file checker into download.py

--- data/download.py
+++ data/download.py
@@ -1,9 +1,22 @@
 import requests
+from scripts.download_batches import NATIVE_BIRDS, BATCH_DIR
+import os
 
 class XenoCantoClient:
     def __init__(self, api_key):
         self.api_key = api_key
 
+    def check_downloaded():
+        birds_to_process = []
+        for bird in NATIVE_BIRDS:
+            batch_file = os.path.join(BATCH_DIR, f"{bird}_batch.npy")
+            if not os.path.exists(batch_file):
+                birds_to_process.append(bird)
+        if not birds_to_process:
+            print("All bird batches already exist. Skipping batch creation.")
+        else:
+            return birds_to_process
+    
     def get_recordings(self, bird_name):
         # pulls bird calls from xeno canto API
         recording_list = []
