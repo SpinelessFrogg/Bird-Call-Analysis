a875893a042d7bdcea80214888090e73327e9630
Caleb <spinelessfrog@gmail.com>
2026-02-09 23:15:51 -0600

    First model hit about 30-50% accuracy. Raising epochs to 50, adding noise, and removing padding

--- training/training.py
+++ training/training.py
@@ -1,12 +1,14 @@
 from sklearn.utils.class_weight import compute_class_weight
 import numpy as np
 from keras.callbacks import EarlyStopping
+from keras.models import load_model
+from config import MODEL_DIR
 
 def train_model(model, spec_train, labels_train):
      model.fit(
         spec_train, labels_train,
         validation_split=0.1,
-        epochs=20,
+        epochs=50,
         batch_size=16,
         class_weight = weight_classes(labels_train),
         callbacks=EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
@@ -22,7 +24,7 @@ def weight_classes(labels_train):
     return class_weights
 
 def evaluate_model(spec_test, labels_test, model):
-    # model = load_model("birdcall_classify_model.keras")
+    model = load_model(f"{MODEL_DIR}bird_model.keras")
     test_loss, test_accuracy = model.evaluate(spec_test, labels_test)
     print(f"Test accuracy: {test_accuracy:.3f}")
     print(f"Test loss: {test_loss:.3f}")
