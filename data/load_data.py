9f7d36f1d4d480e45875380197939bc9889f4c96
Caleb <spinelessfrog@gmail.com>
2026-02-09 19:48:39 -0600

    more bugfixes

--- data/load_data.py
+++ data/load_data.py
@@ -2,13 +2,13 @@ import os
 import numpy as np
 from pathlib import Path
 
-def load_spectrogram_batches(dir="batch_data"):
+def load_spectrogram_batches(dir="data/batches/"):
         specs = []
         labels = []
 
         dir = Path(dir)
         for file in dir.glob("*_batch.npy"): # gets every directory file (object) ending in _batch
-            species = file.split("_batch.npy")[0] # yoinks species name from file name 
+            species = file.stem.split("_batch.npy")[0] # yoinks species name from file name 
             batch = np.load(file, allow_pickle=True)
 
             specs.extend(batch) # adds all spectrograms to specs
