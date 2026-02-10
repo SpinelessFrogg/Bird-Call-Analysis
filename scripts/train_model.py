947bf1a5cd9b315e5fa836417f6fb142f98acd2d
Caleb <spinelessfrog@gmail.com>
2026-02-09 20:09:35 -0600

    added MODEL_DIR const and filled train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -2,10 +2,21 @@ from data.load_data import load_spectrogram_batches
 from preprocessing.dataset_builder import DatasetBuilder
 from training.model import create_model
 from training.training import train_model
-from config import BATCH_DIR
+from config import MODEL_DIR
 
 def main():
-    pass
+    specs, labels = load_spectrogram_batches()
+
+    builder = DatasetBuilder(specs, labels)
+
+    X, y = builder.prepare()
+    spec_train, spec_test, labels_train, labels_test = builder.split(X, y)
+
+    model = create_model(spec_train.shape[1:], spec_test.shape[1])
+
+    train_model(model, spec_train, spec_test, labels_train, labels_test)
+
+    model.save(MODEL_DIR / "bird_model.keras")
 
 if __name__ == "__main__":
     main()
