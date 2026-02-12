76eba2203b9d138a315162f4e482fac8cbcf4d1e
Caleb <spinelessfrog@gmail.com>
2026-02-12 16:11:24 -0600

    separated noise augmentation from training vs evaluation

--- api/main.py
+++ api/main.py
@@ -11,5 +11,8 @@ app = FastAPI()
 model_name = "2-10-26_fixedwidth_extra_conv.keras"
 model = load_model(f"{MODEL_DIR}{model_name}")
 
-
 CLASS_NAMES = np.load(f"{MODEL_DIR}class_names.npy")
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
