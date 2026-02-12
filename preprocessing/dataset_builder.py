017064c4feaa03328bdfe482a2701e230231e591
Caleb <spinelessfrog@gmail.com>
2026-02-12 15:59:04 -0600

    starter fastapi code and added class_names.npy for easy access without disrupting pipeline

--- preprocessing/dataset_builder.py
+++ preprocessing/dataset_builder.py
@@ -30,25 +30,6 @@ class DatasetBuilder:
             random_state=42
         )
 
-    def _pad_to_median_width(self, batch):
-        if batch.size == 0:
-            raise ValueError("Batch is empty")
-        widths = [spec.shape[1] for spec in batch]
-        if not widths:  # Check if widths is empty
-            raise ValueError("No widths found in the batch.")
-        target_w = int(np.median(widths))
-
-        padded = []
-        for spec in batch:
-            h, w = spec.shape
-            # Trim or pad width
-            if w > target_w:
-                spec = spec[:, :target_w]
-            else:
-                spec = np.pad(spec, ((0, 0), (0, target_w - w)), mode="constant")
-            padded.append(spec)
-        return np.array(padded, dtype=np.float32)
-
     def _normalize(self, batch):
         # Normalize each spectrogram
         mean = np.mean(batch)
