65b05dc357f0b103c0473f0fce9d2ec5f7b62fc9
Caleb <spinelessfrog@gmail.com>
2026-02-13 11:39:37 -0600

    left class self code in fix_width and normalize. Also forgot to save file before preivous push

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, UploadFile, File
 import numpy as np
 from keras.models import load_model
-import io
 from preprocessing.audio import decode_audiosegment
 from preprocessing.pipeline import waveform_to_melspec, prepare_single
 from config import MODEL_DIR
+from pydub import AudioSegment
+from io import BytesIO
+
 
 app = FastAPI()
 
@@ -18,8 +20,18 @@ async def predict(file: UploadFile = File(...)):
     contents = await file.read()
 
     try:
-        samples, sr = decode_audiosegment(contents)
+        audio = AudioSegment.from_file(BytesIO(contents), format="mp3")
+        samples, sr = decode_audiosegment(audio)
         spec = waveform_to_melspec(samples, sr)
         spec = prepare_single(spec)
     except Exception as e:
         return {"error": str(e)}
+
+    pred = model.predict(spec)
+    pred_class = int(np.argmax(pred))
+    confidence = float(np.max(pred))
+    
+    return {
+        "species": CLASS_NAMES[pred_class],
+        "confidence": confidence
+    }
